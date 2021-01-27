92603020921b75ba417450288d86c6bf7240c03a
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-01-27 20:31:01 +0200

    Initialization of project

---
+++ PathFinder/PathFinder/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PathFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(args[1]);
+            Console.ReadKey();
+
+        }
+    }
+}
