c5f07354a2bc264e27e7c413d84f403494de4e25
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-01-27 20:51:59 +0200

    Create required empty classes

--- PathFinder/PathFinder/Program.cs
+++ PathFinder/PathFinder/Program.cs
@@ -2,14 +2,27 @@ using System;
 
 namespace PathFinder
 {
+    /// <summary>
+    /// This class is the driver for the program.
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            // check that user provided input file
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please provide input file!");
+                return -1;
+            }
             
-            Console.WriteLine(args[1]);
-            Console.ReadKey();
+            // read map content into two dimensional array
 
+            // find the shortest path (the number of trees along it)
+
+            // print the result for the user
+
+            return 0;
         }
     }
 }
